815445d7deb5ddcccf3b5d8e94b23f19686427a4
Charles Cabergs <me@cacharle.xyz>
2026-03-07 22:36:27 +0100

    Add autobahn test file

---
+++ autobahn.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# docker run -it --rm \
+#   -v "${PWD}/config:/config" \
+#   -v "${PWD}/reports:/reports" \
+#   --network host \
+#   crossbario/autobahn-testsuite \
+#   wstest -m fuzzingclient -s /config/fuzzingclient.json
+
+docker run -it --rm \
+  -v $(pwd):/config \
+  --network=host \
+  crossbario/autobahn-testsuite \
+  wstest -m fuzzingclient -s /config/autobahn.json
